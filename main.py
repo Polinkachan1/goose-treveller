237fcb2a6b5590956eab355049be146a215416b2
polin <polinaandr123@yandex.com>
2023-01-01 13:39:42 +0300

    add button Instructions and button Change_Fon on Start_screen

--- main.py
+++ main.py
@@ -4,11 +4,13 @@ import sys
 
 pygame.init()
 
+
+
 player = None
 step = 10
 FPS = 60
-width = 1000
-height = 800
+width = 800
+height = 600
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Goose-adventurer")
 clock = pygame.time.Clock()
@@ -23,8 +25,40 @@ def load_image(name: str, color_key: int = None):
         raise SystemExit(message)
     return image
 
-class Button:
+
+class Instructions:
     pass
+
+
+class Changing_Fon:
+    pass
+
+
+# white color
+color = (255, 255, 255)
+
+# light shade of the button
+color_light = (170, 170, 170)
+
+# dark shade of the button
+color_dark = (100, 100, 100)
+
+# stores the width of the
+# screen into a variable
+width = screen.get_width()
+
+# stores the height of the
+# screen into a variable
+height = screen.get_height()
+
+# defining a font
+smallfont = pygame.font.SysFont('Corbel', 35)
+
+# rendering a text written in
+# this font
+text_1 = smallfont.render('start', True, color)
+text_2 = smallfont.render('Instructions', True, color)
+text_3 = smallfont.render('Change Fon', True, color)
 
 def start_screen():
     fon = pygame.transform.scale(load_image('start_screen.jpg'), (width, height))
@@ -34,11 +68,48 @@ def start_screen():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
-            elif event.type == pygame.KEYDOWN or \
-             event.type == pygame.MOUSEBUTTONDOWN:
-                return  # начинаем игру
-        pygame.display.flip()
-        clock.tick(FPS)
+            # checks if a mouse is clicked
+            if event.type == pygame.MOUSEBUTTONDOWN:
+
+                # if the mouse is clicked on the
+                # button the game is terminated
+                if width / 2.5 <= mouse[0] <= width / 2.5 + 140 and height / 1.1 <= mouse[1] <= height / 1.1 + 40:
+                    return
+                elif width / 11 <= mouse[0] <= width / 11 + 140 and height / 1.1 <= mouse[1] <= height / 1.1 + 40:
+                    Instructions()
+
+                elif width / 1.5 <= mouse[0] <= width / 1.5 + 140 and height / 1.1 <= mouse[1] <= height / 1.1 + 40:
+                    Changing_Fon()
+
+
+        # stores the (x,y) coordinates into
+        # the variable as a tuple
+        mouse = pygame.mouse.get_pos()
+        # if mouse is hovered on a button it
+        # changes to lighter shade
+        if width / 2.5 <= mouse[0] <= width / 2.5 + 140 and height / 1.1 <= mouse[1] <= height / 1.1 + 40:
+            pygame.draw.rect(screen, color_light, [width / 2.5, height / 1.1, 140, 40])
+
+        else:
+            pygame.draw.rect(screen, color_dark, [width / 2.5, height / 1.1, 140, 40])
+        # second_button Instructions
+        if width / 11 <= mouse[0] <= width / 11 + 140 and height / 1.1 <= mouse[1] <= height / 1.1 + 40:
+            pygame.draw.rect(screen, color_light, [width / 11, height / 1.1, 180, 40])
+
+        else:
+            pygame.draw.rect(screen, color_dark, [width / 11, height / 1.1, 180, 40])
+        # third-button Change Fon
+        if width / 1.5 <= mouse[0] <= width / 1.5 + 140 and height / 1.1 <= mouse[1] <= height / 1.1 + 40:
+            pygame.draw.rect(screen, color_light, [width / 1.5, height / 1.1, 170, 40])
+
+        else:
+            pygame.draw.rect(screen, color_dark, [width / 1.5, height / 1.1, 170, 40])
+        # superimposing the text onto our button
+        screen.blit(text_1, (width / 2.5 + 40, height / 1.1))  # first button
+        screen.blit(text_2, (width / 11 + 10, height / 1.1))  # second button
+        screen.blit(text_3, (width / 1.5, height / 1.1))  # third button
+        # updates the frames of the game
+        pygame.display.update()
 
 
 start_screen()
